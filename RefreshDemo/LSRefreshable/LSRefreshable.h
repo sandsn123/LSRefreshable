edce413b186761e114460b72f0c3b5844829190b
sai <330770597@qq.com>
2023-10-17 10:29:26 +0800

    improve README

---
+++ RefreshDemo/LSRefreshable/LSRefreshable.h
@@ -0,0 +1,18 @@
+//
+//  LSRefreshable.h
+//  LSRefreshable
+//
+//  Created by czi on 2023/10/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LSRefreshable.
+FOUNDATION_EXPORT double LSRefreshableVersionNumber;
+
+//! Project version string for LSRefreshable.
+FOUNDATION_EXPORT const unsigned char LSRefreshableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LSRefreshable/PublicHeader.h>
+
+
